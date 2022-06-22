ee7bfd063cff777305956a68de3df80cf58e4656
SheshkoPavel <derzurb@mail.ru>
2022-06-22 16:33:32 +0300

    feat: add solving task reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = n * -1;
+    }
+    let last, result = 0;
+
+    while( n !==0 ){
+        last = n % 10;  
+        result = (result * 10) + last  ;
+        n = Math.floor(n/10);
+    }    
+    return result;
 }
+
